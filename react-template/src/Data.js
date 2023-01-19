124acdcefc31a8734d603b6b41cd72f52b5cec08
shalu <shalu.kumari@publicissapient.com>
2023-01-19 17:04:56 +0530

    footer file added

---
+++ react-template/src/Data.js
@@ -0,0 +1,26 @@
+import './index.css';
+
+function Data() {
+    return (
+        <form action="/action_page.php">
+            <label for="fname">First name:</label><br/>
+                <input type="text" id="fname" name="fname" value="John"/><br/>
+                    <label for="lname">Last name:</label><br/>
+                        <input type="text" id="lname" name="lname" value="Doe"/><br/><br/>
+                            <input type="submit" value="Submit"/>
+                            </form>
+                            );
+}
+
+                            export default Data;
+
+
+
+
+
+
+
+
+
+
+
